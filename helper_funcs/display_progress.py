b9660cc741c4c8b021ccd5af7ee26ead85cfaeea
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-27 14:22:42 +0530

    Update display_progress.py

--- helper_funcs/display_progress.py
+++ helper_funcs/display_progress.py
@@ -14,8 +14,6 @@ import time
 
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
-    from sample_config import Config 
-else:
     from configs import Config
 
 # the Strings used for this "thing"
