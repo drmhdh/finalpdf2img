bd1f53adef03c38c0e36498137c9c80329ab7c77
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-27 20:53:30 +0530

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -2595,7 +2595,7 @@ async def answer(client, callbackQuery):
             await bot.edit_message_text(
                 chat_id = callbackQuery.message.chat.id,
                 message_id = callbackQuery.message.message_id,
-                text = "`Downloading your pdf..⏳`"
+                text = "`Downloading your pdf..🪴`"
             )
             
             await bot.download_media(
