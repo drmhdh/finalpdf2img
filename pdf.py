cd114453b284ec7e61ebcebc31bc778518483d1f
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-29 22:58:13 +0530

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -1561,7 +1561,7 @@ async def extract(bot, message):
         )
         
 @bot.on_callback_query()
-async def answer(client: bot, callbackQuery):
+async def answer(client: bot, callbackQuery: CallbackQuery):
     clicked = callbackQuery.message.from_user.id
     try:
         typed = callbackQuery.message.reply_to_message.from_user.id
