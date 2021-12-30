c74b260342cb8317958363d475dc6192d4745ce6
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-30 06:37:02 +0530

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -1868,7 +1868,7 @@ async def answer(client: bot, callbackQuery: CallbackQuery):
             try:
                 await bot.delete_messages(
                     chat_id = callbackQuery.message.chat.id,
-                    message_ids = callbackQuery.message.message_id
+                    message_ids = [callbackQuery.message.message_id, callbackQuery.message.reply_to_message.message_id]
                 )
                 return
         
