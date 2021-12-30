7da89a0293819af6646f49edf0afb6e3ff09a4b3
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-30 13:24:37 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -228,3 +228,17 @@ class Presets(object):
     CAPTION_TXT = """{}\n\n<b>😉Credits:</b><a href='https://t.me/dent_tech_for_books'><b> Dent Tech Reference Room</b></a>"""
     THUMB_URL = """https://telegra.ph/file/60706bd59c0829ed2f76f.jpg"""   
     
+    WELCOME_TXT = "<b>Hello.. {}</b>\n<i>I can compress the size of pdf docs. Send me a pdf document to see " \
+                  "the magic !</i> "
+    INVALID_FORMAT = "<b>Error:</b>\n<i>I can only compress pdf documents. Please make it sure, you have given me a " \
+                     "valid document. Try again...</i>"
+    WAIT_MESSAGE = "⌛️ <b>Processing</b> ⌛"
+    DOWNLOAD_MSG = "⌛️ <b>Downloading</b> ⌛"
+    UPLOAD_MSG = "⌛️ <b>Uploading</b> ⌛"
+    FINISHED_BAR = "◼️"
+    UN_FINISHED_BAR = "◻️"
+    FINISHED_DL = "<b>Success !</b>\n<i>Document downloaded successfully.</i>"
+    START_COMPRESSING = "⌛️ <b>Processing</b>  ⌛\n<i>Trying to compress the document.</i>"
+    FINISHED_JOB = "<b>Success</b> ✅ \n\n<i>Size before job: {}\nSize after job: {}\n\nCredits: </i><a " \
+                   "href='https://github.com/m4mallu'><b> @M4Mallu</b></a> "
+    JOB_ERROR = "<b>Error:</b>\n<i>Something went wrong ! Process exited</i>"
