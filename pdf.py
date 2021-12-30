9c5043be68d4b52b3645ca0595001d2f47b5a087
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-30 18:29:55 +0530

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -168,7 +168,7 @@ async def compress_pdf(bot, m: Message):
         document=size_path[1],
         reply_to_message_id=m.reply_to_message.message_id,
         #document=file_name,
-        caption=(m.reply_to_message.caption if m.reply_to_message.caption else file_name /
+        caption=(m.reply_to_message.caption if m.reply_to_message.caption else {file_name} /
                 Presets.FINISHED_JOB.format(initial_size, compressed_size) /
                 "Credits:@dent_tech_for_books"),
                 
