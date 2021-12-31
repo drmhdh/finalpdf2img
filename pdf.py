a9b0aa0fde80452fe657ffcbbd888a590df3ad2c
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2022-01-01 00:42:09 +0530

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -1285,7 +1285,7 @@ async def extract(bot, message):
                                 text='`Analysing Your PDF...🤹`',
                                 reply_to_message_id=message.reply_to_message.message_id
                             )
-                            KUNTHAM= message.reply_to_message.message_id
+                            message.reply_to_message
                             await bot.send_message(
                                 message.chat.id,
                                 text = "⚙️Conversion Going to Initiate..!!",
@@ -1303,7 +1303,7 @@ async def extract(bot, message):
                                     ]
                                 )
                             ) 
-                            KUNTHAM[message.chat.id] = message.reply_to_message.document.file_id
+                            PDF2IMG[message.chat.id] = message.reply_to_message.document.file_id
                             
                             
                         
@@ -1575,7 +1575,7 @@ async def answer(client: bot, callbackQuery: CallbackQuery):
                 #if not os.path.isdir(download_location):
                 #os.makedirs(download_location)
                 the_real_download_location = await bot.download_media(
-                KUNTHAM[message.chat.id] = message.reply_to_message.document.file_id[callbackQuery.message.chat.id],
+                PDF2IMG[message.chat.id],
                 f"{callbackQuery.message.message_id}/pdf.pdf",
                 )
                 progress=progress_for_pyrogram,
