45dbe339ad6027dbc2fd2a577c1ee5885783e369
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2022-01-02 15:38:26 +0530

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -104,7 +104,8 @@ async def ocr(bot, msg):
         else:
             return await msg.reply("`Either the lang code is wrong or the lang is not supported.`", parse_mode='md')
     message = await msg.reply("`Downloading and Extracting...`", parse_mode='md')
-    image = await msg.download(
+    #image = await msg.download(
+    image = await bot.download_media(
         #message=msg,
         file_name=download_location)
     img = Image.open(download_location)
