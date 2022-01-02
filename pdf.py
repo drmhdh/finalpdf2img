9c699fcf590a520b1607de772a787ecaf543547d
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2022-01-02 21:55:30 +0530

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -132,7 +132,7 @@ async def ocr(bot, message):
     )
     text = pytesseract.image_to_string(img, lang=f"{lang_code.text}")
     try:
-        await message.reply_to_message.reply("🙈 Here is What I could Read From Your Image👇"\n\n  text[:-1], quote=True, disable_web_page_preview=True)
+        await message.reply_to_message.reply("🙈 Here is What I could Read From Your Image👇", text[:-1], quote=True, disable_web_page_preview=True)
     except MessageEmpty:
         return await message.reply_to_message.reply("`Either the image has no text or the text is not recognizable.`", quote=True, parse_mode='md')
     await imgocr.delete()
