084260fb41fda6c45d05427cac455a7ed5846aa0
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2022-01-02 15:17:43 +0530

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -93,7 +93,7 @@ async def ocr(bot, msg):
     lang_code = await bot.ask(msg.chat.id,'`Now send the ISO language code.`\n\n[List of ISO 639-2 language codes](https://en.m.wikipedia.org/wiki/List_of_ISO_639-2_codes)', filters=filters.text, parse_mode='Markdown', disable_web_page_preview=True)
     data_url = f"https://github.com/tesseract-ocr/tessdata/raw/main/{lang_code.text}.traineddata"
     #dirs = r"/app/vendor/tessdata"
-    download_location = Config.DOWNLOAD_LOCATIONS + "/" + "testdata" + "/" + text_{msg.from_user.id} + ".jpg"
+    download_location = Config.DOWNLOAD_LOCATIONS + "/" + "testdata" + "/" + str(text_msg.from_user.id) + ".jpg"
     if not os.path.isdir(download_location):
             os.makedirs(download_location)
     path = os.path.join(download_location, f"{lang_code.text}.traineddata")
