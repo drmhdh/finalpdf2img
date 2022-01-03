31e905063b5e06acaf09c74f8d3a7855bed6a496
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2022-01-03 23:01:02 +0530

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -211,9 +211,9 @@ async def compress_pdf(bot, message):
     message = await msg.edit(Presets.UPLOAD_MSG)
     current_time = time.time()
     #
-    If initial_size - compressed_size > 5:
+    #If initial_size - compressed_size > 5:
     #If initial_size - compressed_size = > 5:
-    #if compressed_size < initial_size:
+    if compressed_size < initial_size:
         await message.reply_to_message.reply_document(
             document=size_path[1],
             reply_to_message_id=message.reply_to_message.message_id,
